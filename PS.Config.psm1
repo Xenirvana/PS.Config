0c14f1916841a76bd07134adf2b67b3eb21b22d8
Nathaniel W. P <85239540+SupernautSFX@users.noreply.github.com>
2021-09-16 05:06:48 +0000

    Update PS.Config.psm1

--- PS.Config.psm1
+++ PS.Config.psm1
@@ -1,13 +1,17 @@
-TRY {
+#region Loading Functions/Classes
+
+<# TRY {
     $script:SETTINGS = (Get-Content (Join-Path $PSScriptRoot 'Settings.jsonc') | ConvertFrom-Json)
 }
 CATCH {
     Write-Error -Message "Failed to load Settings.jsonc"
 }
+#>
 
-# $CLASSES = @(Get-ChildItem -Path $PSScriptRoot\Classes -Recurse -Filter "*.ps1") | Sort-Object Name
-$PRIVATE = @(Get-ChildItem -Path $PSScriptRoot\Private -Recurse -Filter "*.ps1") | Sort-Object Name
-$PUBLIC = @(Get-ChildItem -Path $PSScriptRoot\Public -Recurse -Filter "*.ps1") | Sort-Object Name
+# $CLASSES = @(Get-ChildItem -Path $PSScriptRoot\src\Classes -Recurse -Filter "*.ps1") | Sort-Object Name
+$PRIVATE = @(Get-ChildItem -Path $PSScriptRoot\src\Private -Recurse -Filter "*.ps1") | Sort-Object Name
+$PUBLIC = @(Get-ChildItem -Path $PSScriptRoot\src\Public -Recurse -Filter "*.ps1") | Sort-Object Name
+$Script:CONFIGURATION = @(Get-ChildItem -Path $PSScriptRoot\.config -Recurse -Filter "*.ps1") | Sort-Object Name
 
 <# ForEach ($Class in $CLASSES) {
     TRY {
@@ -38,4 +42,16 @@ ForEach ($Function in $PUBLIC) {
     }
 }
 
+ForEach ($ConfigScript in $Script:CONFIGURATION) {
+    TRY {
+        . $ConfigScript.FullName
+        Write-Verbose -Message ("Successfully loaded Module Configuration Script {0}" -F $ConfigScript.FullName)
+    }
+    CATCH {
+        Write-Error -Message ("Failure to load Module Configuration Script {0}: {1}" -F $ConfigScript.FullName, $_)
+    }
+}
+
 Export-ModuleMember -Function $PUBLIC.BaseName
+
+#endregion
